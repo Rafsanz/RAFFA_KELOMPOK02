49d1b95ac19fb00657895a849df9128e803dbaa3
Rafsanz <rafsanjaniraffa14@gmail.com>
2025-10-04 06:54:50 +0700

    ini buat praktikum bang

---
+++ Modul4_Kel02/java/Modul4_Kel02/src/contohClass.java
@@ -0,0 +1,13 @@
+public class contohClass {
+    public void menyapa() {
+        System.out.println("Haloo!!");
+    }
+
+    public String perkenalan(String nama, String kota, String hobi) {
+        return "Namaku " + nama + ", Aku dari " + kota + " dan hobiku adalah " + hobi;
+    }
+
+    public void printUmur(int umur) {
+        System.out.println("Aku berumur "+ umur + " tahun");
+    }
+}
---
+++ Modul4_Kel02/python/call_method.py
@@ -0,0 +1,7 @@
+from coba2_kel02 import method
+
+obj = method("Raffa", 21120125120007)
+
+obj.intro()
+
+obj.timer(5)
